25134d18f03c51297aedd847e12261b0a4e6f7e7
charroux <benoit.charroux@free.fr>
2016-04-25 21:13:27 +0200

    adding properties

--- src/main/groovy/org/olabdynamics/compose/Application.groovy
+++ src/main/groovy/org/olabdynamics/compose/Application.groovy
@@ -13,6 +13,8 @@ class Application {
 	
 	def language
 	
+	def properties = [:]
+	
 	Input input		// une seule input mais qui peut contenir plusieurs messages
 	Output output	// une seule output mais qui peut contenir plusieurs messages
 
